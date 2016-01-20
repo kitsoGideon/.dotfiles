822c3fbe32bd3460f7bdd36f33f4793fde53fe4e
kitso gideon namane <kitsoGideon@users.noreply.github.com>
2016-01-20 14:28:50 +0200

    Update devEnv.sh

--- env_setup/devEnv.sh
+++ env_setup/devEnv.sh
@@ -2,7 +2,7 @@
 
 mkdir .gitClones
 wget -o- https://toolbelt.heroku.com/install.sh | sh
-apt-get install git curl python-pip
+apt-get install git curl python-pip zsh
 \curl -sSL https://get.rvm.io | bash
 wget --no-check-certificate http://instal.ohmyz.sh -O - | sh
 
