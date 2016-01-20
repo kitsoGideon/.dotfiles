11de8e622a88d24f27d03c2167efdf9a07910cbb
kitso gideon namane <kitsoGideon@users.noreply.github.com>
2016-01-20 13:12:47 +0200

    update devEnv.sh
    add git, curl and rvm

--- env_setup/devEnv.sh
+++ env_setup/devEnv.sh
@@ -3,3 +3,7 @@
 mkdir .gitClones
 wget --no-check-certificate http://instal.ohmyz.sh -O - | sh
 wget -o- https://toolbelt.heroku.com/install.sh | sh
+apt-get install git curl python-pip
+\curl -sSL https://get.rvm.io | bash
+
+
